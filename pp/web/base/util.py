63c07e8330b79b94cefe77e2987f33c4bfbd63d6
Oisin Mulvihill <oisin.mulvihill@gmail.com>
2012-11-22 16:32:00 +0000

    added a decorator to require at least a 'logged in user'. This can be passed to the view_config as the decorator=login_required.

--- pp/web/base/util.py
+++ pp/web/base/util.py
@@ -1,12 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Web helper functions.
+
+"""
 import logging
-import pprint
+#import pprint
 
 from pyramid.httpexceptions import HTTPFound, HTTPUnauthorized
 from repoze.what import predicates
 
 
-def get_log():
-    return logging.getLogger('pp.web.base.util')
+def get_log(extra=None):
+    m = "{}.{}".format(__name__, extra) if extra else __name__
+    return logging.getLogger(m)
 
 
 def redirect(url):
@@ -24,3 +30,33 @@ def check_predicate(request, p):
         p.check_authorization(request.environ)
     except predicates.NotAuthorizedError:
         raise HTTPUnauthorized
+
+
+def login_required(request_handler):
+    """A decorator used to require a user to be logged in before
+    access a view.
+
+    @view_config(
+        route_name='survey', request_method='GET', decorator=login_required,
+        accept="text/html", renderer='home.jinja2',
+    )
+
+    """
+    log = get_log('login_required')
+
+    def _fn(context, request):
+        try:
+            log.info("checking / requiring login")
+            p = predicates.not_anonymous(msg='Must be logged in')
+            p.check_authorization(request.environ)
+
+        except predicates.NotAuthorizedError:
+            log.debug("login required.")
+            redirect('/login')
+
+        else:
+            log.debug("logged in.")
+
+        return request_handler(context, request)
+
+    return _fn
